0b2f0762998af9c85c25c07c30e91daf9485ff1e
Jason Ellison <jason.w.ellison@gmail.com>
2022-07-05 19:31:32 -0400

    Bugfix round 1.

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,24 @@
 import { Walkers } from "./Walkers.js"
 import { CityList } from "./CityList.js"
 import { Assignments } from "./Assignments.js"
+import { RegisteredPets } from "./RegisteredPets.js"
 
 const mainContainer = document.querySelector("#container")
 
 const applicationHTML = `
-<h1>DeShawns Dog Walking</h1>
+<h1>DeShawn's Dog Walking</h1>
 <article class="details">
     <section class="detail--column list details__cities">
         <h2>Cities with Service</h2>
         ${CityList()}
     </section>
-    <section class="detail--column list details__cities">
+    <section class="detail--column list details__walkers">
         <h2>Walkers</h2>
         ${Walkers()}
     </section>
-    <section class="detail--column list details__cities">
+    <section class="detail--column list details__pets">
         <h2>Pets</h2>
+        ${RegisteredPets()}
     </section>
 </article>
 
