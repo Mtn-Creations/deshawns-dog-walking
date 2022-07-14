340bd94090a7f5be7c852374cba244a0d3bca6f9
Jason Ellison <jason.w.ellison@gmail.com>
2022-07-14 03:45:58 -0400

    Added click event listener to Pets module

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,42 @@
-import { getPets } from "./database.js"
+import { getPets, getWalkers } from "./database.js"
 
 const pets = getPets()
+const petWalkers = getWalkers()
+
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+
+        const itemClicked = clickEvent.target
+
+        if (itemClicked.id.startsWith("pet")) {
+            const [,petId] = itemClicked.id.split("--")
+
+            for (const pet of pets) {
+                if (pet.id === parseInt(petId)) {
+                    window.alert(`${pet.name} is being walked by ${findPetWalker(pet.walkerId, petWalkers).name}`)  
+                }
+            }
+
+        }
+        
+    }
+)
+
+
+function findPetWalker(_walkerId, _petWalkers) {
+    for (const walker of _petWalkers) {
+        if (_walkerId === walker.id) return walker
+    }
+}
+
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id=pet--${pet.id}>${pet.name}</li>`
     }
 
     petHTML += "</ul>"
