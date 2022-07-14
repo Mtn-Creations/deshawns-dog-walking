f8fa71007c0ac2925da7539f8da27120a75694ba
Jason Ellison <jason.w.ellison@gmail.com>
2022-07-14 02:32:24 -0400

    Bugfix: display when walker services mult. cities

--- scripts/Assignments.js
+++ scripts/Assignments.js
@@ -1,9 +1,10 @@
-import { getPets, getWalkers } from "./database.js"
+import { getCities, getPets, getWalkerCities, getWalkers } from "./database.js"
 
 // Get copy of state for use in this module
 const pets = getPets()
 const walkers = getWalkers()
-
+const cities = getCities()
+const walkerCities = getWalkerCities()
 
 // Function whose responsibility is to find the walker assigned to a pet
 const findPetWalker = (pet, allWalkers) => {
@@ -18,16 +19,33 @@ const findPetWalker = (pet, allWalkers) => {
     return petWalker
 }
 
+// Function to find cities assigned to individual walker
+const findWalkerCities = (walker, cities, walkerCities) => {
+    const _walkerCityNames = []
+
+    for ( const walkerCity of walkerCities) {
+        if (walker.id === walkerCity.walkerId) {
+            for ( const city of cities.filter(city => city.id === walkerCity.cityId) ) {
+                _walkerCityNames.push(city.name)
+            }
+        }
+    }
+
+    return _walkerCityNames
+}
+
+
 export const Assignments = () => {
     let assignmentHTML = ""
     assignmentHTML = "<ul>"
 
     for (const currentPet of pets) {
         const currentPetWalker = findPetWalker(currentPet, walkers)
+        const currentPetWalkerCities = findWalkerCities(currentPetWalker, cities, walkerCities)
         assignmentHTML += `
             <li>
                 ${currentPet.name} is being walked by
-                ${currentPetWalker.name} in ${currentPetWalker.city}
+                ${currentPetWalker.name} in ${currentPetWalkerCities.join(' and ')}
             </li>
         `
     }
@@ -36,4 +54,3 @@ export const Assignments = () => {
 
     return assignmentHTML
 }
-
